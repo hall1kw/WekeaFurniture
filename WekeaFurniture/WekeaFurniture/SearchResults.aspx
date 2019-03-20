6135bc7eca9f5c8144879785f47c876726f9c899
Kenneth Hall <hall1kw@cmich.edu>
2019-03-20 14:01:27 -0400

    Updated DB for search functionality

--- WekeaFurniture/WekeaFurniture/SearchResults.aspx
+++ WekeaFurniture/WekeaFurniture/SearchResults.aspx
@@ -6,15 +6,12 @@
     <asp:DataList ID="dlSearchResults" runat="server" RepeatColumns="5" RepeatDirection="Horizontal" BorderColor="#753649" BorderStyle="Solid" BorderWidth="3px" HorizontalAlign="Center">
         <ItemTemplate>
             <div class="text-left" style=" margin-top: 10px; margin-bottom: 10px; margin-right: 10px; margin-left: 10px;">
-                   
-           
                 <asp:Image ID="Image2" runat="server" Height="200px" ImageUrl='<%# "Images/ProductImages/" + Eval("IMAGE") %>' width="200px" />
                 <br />
                 <asp:HyperLink ID="Label1" runat="server" NavigateUrl='<%# "ProductDetail.aspx?ID="+Eval("ID") %>' Text='<%# Eval("NAME") %>'></asp:HyperLink>
                 <br />
                 <div style="text-align: right">
                     $<asp:Label ID="Label2" runat="server" Text='<%# Eval("PRICE") %>'></asp:Label>
-
                 </div>
                 
             </div>
--- WekeaFurniture/WekeaFurniture/SearchResults.aspx.cs
+++ WekeaFurniture/WekeaFurniture/SearchResults.aspx.cs
@@ -14,21 +14,45 @@ public partial class _Default : System.Web.UI.Page
         {
             string id = Request.QueryString["PID"];
             string name = Request.QueryString["Name"];
-            string minPrice = Request.QueryString["minPrice"];
-            string maxPrice = Request.QueryString["maxPrice"];
-            string room = Request.QueryString["room"];
-            string category = Request.QueryString["category"];
+            string minPrice = Request.QueryString["minP"];
+            string maxPrice = Request.QueryString["maxP"];
+            string room = Request.QueryString["Rm"];
+            string category = Request.QueryString["Cat"];
 
+            /*
+             * KH - Construction of the SQL query.
+             * ID and Name are or values and will return all that match either criteria.  
+             * Pricing options are and statements and will restrict responses to match their values.
+            */
             StringBuilder command = new StringBuilder("SELECT * FROM PRODUCTS WHERE ");
-
+            
             if (!id.Equals(""))
             {
                 command.Append("ID=" + id);
             }
             if (!name.Equals("") && command.ToString().Equals("SELECT * FROM PRODUCTS WHERE "))
             {
-                command.Append("NAME LIKE'%" + name +"%'");
+                command.Append(" NAME LIKE'%" + name +"%'");
+            } else if (!name.Equals(""))
+            {
+                command.Append(" OR NAME LIKE'%" + name + "%'");
             }
+            if (!minPrice.Equals("") && command.ToString().Equals("SELECT * FROM PRODUCTS WHERE "))
+            {
+                command.Append("Price >=" + minPrice);
+            } else if (!minPrice.Equals(""))
+            {
+                command.Append(" AND Price >=" + minPrice);
+            }
+            if (!maxPrice.Equals("") && command.ToString().Equals("SELECT * FROM PRODUCTS WHERE "))
+            {
+                command.Append(" Price <=" + maxPrice);
+            }
+            else if (!maxPrice.Equals(""))
+            {
+                command.Append(" AND Price <=" + maxPrice);
+            }
+            if (room.Contains("Kitchen"))
 
             dlSearchResults.DataSource = DataAccess.selectQuery(command.ToString());
             dlSearchResults.DataBind();
