e15abaf213d3ef69d94d33c5d531cb34323e4a7e
Kenneth Hall <hall1kw@cmich.edu>
2019-03-20 12:10:21 -0400

    Working on Search results

--- WekeaFurniture/WekeaFurniture/SearchResults.aspx.cs
+++ WekeaFurniture/WekeaFurniture/SearchResults.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,13 +10,31 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        string id = Request.QueryString["id"];
-        string name = Request.QueryString["name"];
-        string minPrice = Request.QueryString["minPrice"];
-        string maxPrice = Request.QueryString["maxPrice"];
-        string room = Request.QueryString["room"];
-        string category = Request.QueryString["category"];
+        if (!IsPostBack)
+        {
+            string id = Request.QueryString["PID"];
+            string name = Request.QueryString["Name"];
+            string minPrice = Request.QueryString["minPrice"];
+            string maxPrice = Request.QueryString["maxPrice"];
+            string room = Request.QueryString["room"];
+            string category = Request.QueryString["category"];
+
+            StringBuilder command = new StringBuilder("SELECT * FROM PRODUCTS WHERE ");
+
+            if (!id.Equals(""))
+            {
+                command.Append("ID=" + id);
+            }
+            if (!name.Equals("") && command.ToString().Equals("SELECT * FROM PRODUCTS WHERE "))
+            {
+                command.Append("NAME LIKE '*" + name + "*'");
+            }
+
+            dlSearchResults.DataSource = DataAccess.selectQuery(command.ToString());
+            dlSearchResults.DataBind();
 
 
+
+        }
     }
 }
