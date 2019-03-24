828ed2deba9828b762aa3e990005982a40e44bbc
Kenneth Hall <hall1kw@cmich.edu>
2019-03-24 17:51:23 -0400

    Home replaced

---
+++ WekeaFurniture/WekeaFurniture/Home.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Home : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string id = Request.QueryString["ID"];
+            dlFeatured.DataSource = DataAccess.selectQuery("SELECT TOP 6 * FROM PRODUCTS WHERE FEATURED = 1 ORDER BY NEWID()");
+            dlFeatured.DataBind();
+        }
+    }
+}
